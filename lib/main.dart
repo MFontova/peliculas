9a3fb56f8097502eb001ccdfc7b7189431e4fc14
Marc Fontova <mfontovat@gmail.com>
2023-01-08 10:07:26 +0100

    start

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Peliculas',
+      initialRoute: 'homes',
+      routes: {
+        'home': (context) => HomeScreen(),
+        'details': (context) => DetailsScreen()
+      },
+    );
+  }
+}
