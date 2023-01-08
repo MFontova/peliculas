0b3741444cb65b5971d9df67809984ffd26fcf00
Marc Fontova <mfontovat@gmail.com>
2023-01-08 22:18:32 +0100

    117

--- lib/providers/movies_providers.dart
+++ lib/providers/movies_providers.dart
@@ -1,13 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = '6a20af71d4f080ce5133e91982b8e30e';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('MoviesProvider inicializado');
 
     getOnDisplayMovies();
   }
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData['results']);
   }
 }
